71cb226a8b0e5807222c749a506180541fcb44c4
Giovanny <ekeisco@gmail.com>
2012-06-24 18:20:07 -0500

    se actualiza con el estado PERDIO

--- nodejsClient.js
+++ nodejsClient.js
@@ -7,26 +7,31 @@ var client = net.connect(5000, function() {
   console.log('client connected');
   client.write('agares,10101\r\n');
 });
+
 client.on('data', function(data) {
 
   var info = data.toString().split(";");
   switch(info[0]){
-    case "Empezo la partida":
+    case "EMPEZO":
       //leemos el mapa incial y nuestra letra
       bot.letter = info[2];
       bot.updateMap(info[1]);
     break;
-    case "turno":
+    case "TURNO":
       console.log(info[1]);
       bot.updateMap(info[2]);
       var mov= bot.move();
       client.write(mov);
+    break;
+    case "PERDIO":
+      console.log("perdi :'( ");
     break;
     default:
       console.log(info[0]);
     break;
   }
 });
+
 client.on('end', function() {
   console.log('client disconnected');
 });
